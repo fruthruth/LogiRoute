08363de31786f4a96b7ed7e02a2968f4b7c8c557
fruthruth <fernandez.run779@gmail.com>
2026-06-10 00:16:37 -0500

    modificaque controller

--- src/main/java/com/logiroute/logiroute/controller/AuthController.java
+++ src/main/java/com/logiroute/logiroute/controller/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
