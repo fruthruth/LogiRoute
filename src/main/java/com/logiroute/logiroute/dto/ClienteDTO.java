a625a9cb27b5bafd0f623c8ac800b1f29cad24fe
fruthruth <fernandez.run779@gmail.com>
2026-07-11 00:46:10 -0500

    cree clienteDTO

---
+++ src/main/java/com/logiroute/logiroute/dto/ClienteDTO.java
@@ -0,0 +1,19 @@
+package com.logiroute.logiroute.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO {
+
+    private Long usuarioId;
+    private String telefono;
+    private String direccion;
+}
